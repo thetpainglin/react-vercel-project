79e9ca620f2523f83334949182b91fbb9ba1d468
thetpainglin <76717297+thetpainglin@users.noreply.github.com>
2024-02-11 16:15:40 +0630

    Update API.js

--- frontend/src/setting/API.js
+++ frontend/src/setting/API.js
@@ -1,3 +1,3 @@
 //export const API_URL = "http://localhost:3000/api/";
 //export const API_URL = "deploying-mern-api.vercel.app/api/";
-export const API_URL = "https://deploying-express-api.vercel.app/api/";
+export const API_URL = "https://backend-express-api.vercel.app/api/";
