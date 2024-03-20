14f13c011374cb987694f5d26c26737822a49ff8
thetpainglin <htikemyothan85@gmail.com>
2024-03-20 16:26:00 +0630

    Emailing add

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,18 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
-const cors = require('cors');
+let createError = require("http-errors");
+let express = require("express");
+let path = require("path");
+let cookieParser = require("cookie-parser");
+let logger = require("morgan");
+const cors = require("cors");
 
-const mongoose = require('mongoose');
-const {db} = require('./config/database');
+const mongoose = require("mongoose");
+const { db } = require("./config/database");
 
-let auth = require('./middleWare/auth');
-let indexRouter = require('./routes/index');
+let auth = require("./middleWare/auth");
+let indexRouter = require("./routes/index");
 
-{/*for kae Daung Project
+{
+  /*for kae Daung Project
 
   let usersRouter = require('./routes/users');
   let FootballRouter = require('./routes/footballClub');
@@ -19,30 +20,31 @@ let indexRouter = require('./routes/index');
   let PlayerReviewRouter = require('./routes/playerReview');
   */
 }
-{/*
+{
+  /*
   for ecommerce project
 
   let productRouter = require('./Ecomerce_Project/route/productRouter');
   let bannerRouter = require('./Ecomerce_Project/route/bannerRouter');
   */
 }
-let productRouter = require('./Ecomerce_Project/route/productRouter');
-let bannerRouter = require('./Ecomerce_Project/route/bannerRouter');
+let productRouter = require("./Ecomerce_Project/route/productRouter");
+let bannerRouter = require("./Ecomerce_Project/route/bannerRouter");
 
+let emailRouter = require("./routes/email");
 //const {entry} = require("react-circle-slider/webpack.config");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 /*
 *
@@ -54,23 +56,27 @@ app.use(express.static(path.join(__dirname, 'public')));
     }
 * */
 
-app.use(cors(
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+  })
+);
+app.options("*", cors());
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Mongodb connected!"))
+  .catch((err) => console.log(err));
+
+app.use("/", indexRouter);
+
 {
-  "origin": "*",
-  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-  "preflightContinue": false
-}
-));
-app.options('*',cors());
-
-mongoose.connect(db,{
-  useNewUrlParser : true,
-  useUnifiedTopology : true,
-}).then(()=>console.log("Mongodb connected!")).catch(err => console.log(err));
-
-app.use('/', indexRouter);
-
-{/* for kae Daung Project
+  /* for kae Daung Project
 
 //auth.verifyUserToken, in /api/movies/
 
@@ -81,29 +87,33 @@ app.use('/', indexRouter);
    */
 }
 
-{/*
+{
+  /*
   for ecommerce project
 
   app.use('/api/products', productRouter);
   app.use('/api/banner', bannerRouter);
-*/}
-app.use('/api/products', productRouter);
-app.use('/api/banner', bannerRouter);
+*/
+}
+app.use("/api/products", productRouter);
+app.use("/api/banner", bannerRouter);
+
+app.use("/email-send", emailRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
---
+++ backend/routes/email.js
@@ -0,0 +1,57 @@
+var express = require("express");
+var router = express.Router();
+
+const nodemailer = require("nodemailer");
+
+/* GET users listing. */
+router.post("/", (req, res) => {
+  const formData = req.body;
+
+  console.log("email data got => ", formData);
+
+  const emailUser = "zeroheromrthet@gmail.com";
+  const emailPass = "cowzfeuqphgpnexl";
+
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    // host: "smtp.gmail.com",
+    // port: 465,
+    // secure: true,
+    auth: {
+      user: emailUser,
+      pass: emailPass,
+    },
+  });
+
+  const mailOptions = {
+    from: "htikemyothan85@gmail.com",
+    to: emailUser,
+    subject: formData.subject,
+    html: formData.html,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error sending email:", error);
+      res.status(500).send("Error sending email");
+    } else {
+      console.log("Email sent:", info.response);
+      res.status(200).send("Email sent successfully");
+    }
+  });
+});
+
+module.exports = router;
