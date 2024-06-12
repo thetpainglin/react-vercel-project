bb366e380ccdee3437d7deb9d37181843f682d25
thetpainglin <76717297+thetpainglin@users.noreply.github.com>
2024-06-12 17:46:08 +0630

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,23 +11,13 @@ import "./App.css";
 import "./ecomerce/ecomerce.css";
 import allCodeTest from "./TestCode/AllCodeTest";
 import ReducerDemo from "./TestCode/ReducerDemo";
-import HelmetSEOTest from "./HelmetSEOTest";
 
 function App() {
 
 let user = useSelector(userSelector);
   return (
     < div className="bg-white">
-    <HelmetSEOTest
-        title="RI Institute"
-        description="provide a Unique
-        learning. Students are surrounded by abroad study possibly
-        and foreign job opportunity on many diverse areas.
-        We offer academia, technology, foreign language, sport and
-        community responsibility."
-        name="fb or twitter"
-        type="website"
-      />
+  
         {/*
             football project code
 
@@ -46,7 +36,7 @@ let user = useSelector(userSelector);
         <Success/>
 
         */}
-
+ <TestCode/>
         <EcomerceProjectIndex/>
 
     </div>
